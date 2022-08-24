fac90bc1b69a7f8a3de4654bfed50be8e51c2e36
ebauschatz <eaenglish113@gmail.com>
2022-08-24 15:06:56 -0500

    docs: additional docstrings and style tweaks

--- OOP_Labs/Classes_And_Objects/alarm_clock.py
+++ OOP_Labs/Classes_And_Objects/alarm_clock.py
@@ -1,5 +1,11 @@
+"""Class file for alarm clocks
+
+An alarm clock can be used to display the current time or optionally sound an alarm 
+at a specific time.
+"""
+
 class AlarmClock:
-    '''Representation of an alarm clock
+    """Representation of an alarm clock
 
     Instance Variables:
         current_time:
@@ -16,37 +22,47 @@ class AlarmClock:
             Sets a new value for the alarm being turned on or not
         set_alarm_time:
             Sets a new value for the alarm_time and displays the time to the console
-    '''
+    """
     def __init__(self):
+        """Initializes a customer
+        
+        Instance Variables:
+            current_time:
+                string in 24-hour time representing the current time displayed on the clock
+            alarm_enabled:
+                bool representing if the alarm will sound or not at the alarm_time
+            alarm_time:
+                string in 24- hour time representing the time the alarm is set for
+        """
         self.current_time = '00:00'
         self.alarm_enabled = True
         self.alarm_time = '10:00'
 
     def set_current_time(self):
-        '''Sets a new value for current_time baesd on user input and displays the time to the console
+        """Sets a new value for current_time baesd on user input and displays the time to the console
         
         Effects:
             The current_time string is updated to the new value
-        '''
+        """
         new_time = input('Please enter a current time in 24-hour format: ')
         self.current_time = new_time
         print(f'The current time is: {self.current_time}')
 
     def set_is_alarm_enabled(self):
-        '''Sets a new value for the alarm being turned on or not based on user input
+        """Sets a new value for the alarm being turned on or not based on user input
         
         Effects:
             The alarm_enabled bool is updated to the new value
-        '''
+        """
         new_enabled_status = input('Please enter True to turn the alarm on or False to turn it off: ')
         self.alarm_enabled = new_enabled_status
 
     def set_alarm_time(self):
-        '''Sets a new value for the alarm_time based on user input and displays the time to the console
+        """Sets a new value for the alarm_time based on user input and displays the time to the console
         
         Effects:
             The alarm_time string is updated to the new value
-        '''
+        """
         new_alarm_time = input('Please enter an alarm time in 24-hour format: ')
         self.alarm_time = new_alarm_time
         print(f'The alarm time is set for: {self.alarm_time}')
--- OOP_Labs/Classes_And_Objects/customer.py
+++ OOP_Labs/Classes_And_Objects/customer.py
@@ -1,7 +1,13 @@
+"""Class file for customers
+
+Customers represent individual entities who wish to purchase products. 
+Each customer will have a name and be associated to one shopping cart object.
+"""
+
 from shopping_cart import ShoppingCart
 
 class Customer:
-    '''Representation of a customer who will purchase items
+    """Representation of a customer who will purchase items
 
     Instance Variables:
         name:
@@ -18,27 +24,35 @@ class Customer:
                 The new product is appended to the existing cart
         display_cart:
             Prints the contents of the cart to the console, or a message if the cart is empty
-    '''
+    """
     def __init__(self, name):
+        """Initializes a customer object
+        
+        Instance Variables:
+            name:
+                string representing the name of the customer
+            shopping_cart:
+                shopping cart object associated with the customer
+        """
         self.name = name
         self.shopping_cart = ShoppingCart()
 
     def add_product_to_customer_cart(self, new_product):
-        '''Adds a single product object to the cart via a method on the cart object
+        """Adds a single product object to the cart via a method on the cart object
 
-        Parameters:
+        Args:
             new_product -- a single product object to add to the cart
         Effects:
             The new product is appended to the existing cart
-        '''
+        """
         self.shopping_cart.add_product_to_cart(new_product)
 
     def display_cart(self):
-        '''Prints the contents of the cart to the console, or a message if the cart is empty
+        """Prints the contents of the cart to the console, or a message if the cart is empty
         
         Effects:
             Console output
-        '''
+        """
         if len(self.shopping_cart.products) == 0:
             print('\nThe shopping cart is currently empty.')
         else:
