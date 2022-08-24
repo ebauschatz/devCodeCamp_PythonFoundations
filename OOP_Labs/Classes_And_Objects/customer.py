127a1b9b4d5cc83345e98298390875dfb7a4c209
ebauschatz <eaenglish113@gmail.com>
2022-08-24 14:42:52 -0500

    docs: updating docstrings for methods

--- OOP_Labs/Classes_And_Objects/customer.py
+++ OOP_Labs/Classes_And_Objects/customer.py
@@ -4,8 +4,10 @@ class Customer:
     '''Representation of a customer who will purchase items
 
     Instance Variables:
-        name -- string representing the name of the customer
-        shopping_cart -- shopping cart object associated with the customer
+        name:
+            string representing the name of the customer
+        shopping_cart:
+            shopping cart object associated with the customer
 
     Public Methods:
         add_product_to_customer_cart:
@@ -22,9 +24,21 @@ class Customer:
         self.shopping_cart = ShoppingCart()
 
     def add_product_to_customer_cart(self, new_product):
+        '''Adds a single product object to the cart via a method on the cart object
+
+        Parameters:
+            new_product -- a single product object to add to the cart
+        Effects:
+            The new product is appended to the existing cart
+        '''
         self.shopping_cart.add_product_to_cart(new_product)
 
     def display_cart(self):
+        '''Prints the contents of the cart to the console, or a message if the cart is empty
+        
+        Effects:
+            Console output
+        '''
         if len(self.shopping_cart.products) == 0:
             print('\nThe shopping cart is currently empty.')
         else:
--- OOP_Labs/Classes_And_Objects/shopping_cart.py
+++ OOP_Labs/Classes_And_Objects/shopping_cart.py
@@ -24,13 +24,30 @@ class ShoppingCart:
         self.products = []
 
     def current_products_total_price(self):
+        '''Calculate the sum of all prices from all product objects in the cart
+
+        Returns:
+            float of the price total
+        '''
         total_price = 0
         for product in self.products:
             total_price += product.price
         return total_price
 
     def add_product_to_cart(self, new_product):
+        '''Appends a new product object to the current product list
+
+        Parameters:
+            new_product -- a product object to append
+        Effects:
+            The new product will be appended to the existing products list
+        '''
         self.products.append(new_product)
 
     def clear_cart(self):
+        '''Removes all products from the cart
+        
+        Effects:
+            Resets the products list to an empty list
+        '''
         self.products = []
